bc461259cea08337f6fbc56bf92e1c292a9d6d53
asiedusolo <solomon.asiedu@amalitech.org>
2021-12-10 11:51:24 -0500

    adding autonomousCar class and interfaces, and steeringControl class and interfaces

--- index.ts
+++ index.ts
@@ -1,1 +1,51 @@
 import { getObstacleEvents } from './computer-vision';
+
+interface AutonomousCar {
+    isRunning?: boolean;
+    respond: (events: Events) => void;
+  }
+  
+  interface AutonomousCarProps {
+    isRunning?: boolean;
+  }
+  
+  interface Events {
+    [obstacleDirection: string]: boolean;
+  }
+  
+  interface Control {
+    execute: (command: string) => void;
+  }
+  
+  interface Steering extends Control{
+    turn: (direction: string) => void;
+  }
+  
+  
+  class Car implements AutonomousCar{
+    isRunning;
+  
+    constructor(props: AutonomousCarProps){
+      this.isRunning = props.isRunning;
+    }
+  
+    respond(events: Events){
+      if(!this.isRunning){
+        return
+      }
+    }
+  }
+  
+  class SteeringControl implements Steering {
+    execute(command: string){
+      console.log(`Executing: ${command}`)
+    }
+    turn(direction: string){
+      this.execute(`turn ${direction}`)
+    }
+  }
+  
+  const steering = new SteeringControl();
+  steering.turn("left")
+  const autonomousCar = new Car({isRunning: true});
+  console.log(autonomousCar.respond(getObstacleEvents()))
