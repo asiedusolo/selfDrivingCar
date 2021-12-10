d7c82a639016ba10a799371e3594a8a8046d7ce5
asiedusolo <solomon.asiedu@amalitech.org>
2021-12-10 12:03:58 -0500

    adding logic to let the car take the wheel

--- index.ts
+++ index.ts
@@ -1,51 +1,54 @@
 import { getObstacleEvents } from './computer-vision';
 
 interface AutonomousCar {
-    isRunning?: boolean;
-    respond: (events: Events) => void;
+  isRunning?: boolean;
+  respond: (events: Events) => void;
+}
+
+interface AutonomousCarProps {
+  isRunning?: boolean;
+  steeringControl: Steering;
+}
+
+interface Events {
+  [obstacleDirection: string]: boolean;
+}
+
+interface Control {
+  execute: (command: string) => void;
+}
+
+interface Steering extends Control{
+  turn: (direction: string) => void;
+}
+
+
+class Car implements AutonomousCar{
+  isRunning;
+  steeringControl;
+
+  constructor(props: AutonomousCarProps){
+    this.isRunning = props.isRunning;
+    this.steeringControl = props.steeringControl;
   }
-  
-  interface AutonomousCarProps {
-    isRunning?: boolean;
-  }
-  
-  interface Events {
-    [obstacleDirection: string]: boolean;
-  }
-  
-  interface Control {
-    execute: (command: string) => void;
-  }
-  
-  interface Steering extends Control{
-    turn: (direction: string) => void;
-  }
-  
-  
-  class Car implements AutonomousCar{
-    isRunning;
-  
-    constructor(props: AutonomousCarProps){
-      this.isRunning = props.isRunning;
-    }
-  
-    respond(events: Events){
-      if(!this.isRunning){
-        return
-      }
-    }
-  }
-  
-  class SteeringControl implements Steering {
-    execute(command: string){
-      console.log(`Executing: ${command}`)
-    }
-    turn(direction: string){
-      this.execute(`turn ${direction}`)
+
+  respond(events: Events){
+    if(!this.isRunning){
+      return
     }
   }
-  
-  const steering = new SteeringControl();
-  steering.turn("left")
-  const autonomousCar = new Car({isRunning: true});
-  console.log(autonomousCar.respond(getObstacleEvents()))
+}
+
+class SteeringControl implements Steering {
+  execute(command: string){
+    console.log(`Executing: ${command}`)
+  }
+  turn(direction: string){
+    this.execute(`turn ${direction}`)
+  }
+}
+
+const steering = new SteeringControl();
+steering.turn("left")
+const autonomousCar = new Car({isRunning: true, steeringControl: steering});
+console.log(autonomousCar.respond(getObstacleEvents()))
